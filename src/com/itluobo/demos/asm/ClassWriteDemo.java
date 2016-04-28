b47f77517d987a7260e8b2cd8f24de5fca2aaeee
Zhu Weijie <kenvi.zhu@dianping.com>
2016-04-28 09:45:52 +0800

    add ClassWrite asm

--- src/com/itluobo/demos/asm/ClassWriteDemo.java
+++ src/com/itluobo/demos/asm/ClassWriteDemo.java
@@ -1,35 +1,72 @@
 package com.itluobo.demos.asm;
 
-import jdk.internal.org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
-import java.io.File;
 import java.io.FileOutputStream;
 
-import static jdk.internal.org.objectweb.asm.Opcodes.*;
 
 /**
  * Created by hannahzhang on 16/4/23.
  */
-public class ClassWriteDemo {
+public class ClassWriteDemo implements Opcodes{
 
     public static void main(String[] args) throws Exception{
         ClassWriter cw = new ClassWriter(0);
-        cw.visit(V1_5, ACC_PUBLIC + ACC_ABSTRACT + ACC_INTERFACE,
-                "pkg/Comparable", null, "java/lang/Object",
-                new String[] {"pkg/Mesurable"});
-        cw.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, "LESS", "I",
-                null, new Integer(-1)).visitEnd();
-        cw.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, "EQUAL", "I",
-                null, new Integer(0)).visitEnd();
-        cw.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC, "GREATER", "I",
-                null, new Integer(1)).visitEnd();
-        cw.visitMethod(ACC_PUBLIC + ACC_ABSTRACT, "compareTo",
-                "(Ljava/lang/Object;)I", null, null).visitEnd();
-        byte[] b = cw.toByteArray();
-        File file = new File("/tmp/Comparable.class");
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(b);
-        fos.close();
+        FieldVisitor fv;
+        MethodVisitor mv;
+
+        cw.visit(49,ACC_PUBLIC + ACC_SUPER,
+                "Hello",
+                null,
+                "java/lang/Object",
+                null);
+
+        cw.visitSource("Hello.java",null);
+
+        {
+            mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null,null);
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitMethodInsn(INVOKESPECIAL,
+                    "java/lang/Object",
+                    "<init>",
+                    "()V",
+                    false);
+            mv.visitInsn(RETURN);
+            mv.visitMaxs(1,1);
+            mv.visitEnd();
+        }
+
+        {
+            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC,
+                    "main",
+                    "([Ljava/lang/String;)V",
+                    null,
+                    null);
+            mv.visitFieldInsn(GETSTATIC,
+                    "java/lang/System",
+                    "out",
+                    "Ljava/io/PrintStream;");
+            mv.visitLdcInsn("hello");
+            mv.visitMethodInsn(INVOKESPECIAL,
+                    "java/io/PrintStream",
+                    "println",
+                    "(Ljava/lang/String;)V",
+                    false);
+            mv.visitInsn(RETURN);
+            mv.visitMaxs(2, 1);
+            mv.visitEnd();
+
+
+        }
+
+        cw.visitEnd();
+
+        FileOutputStream out = new FileOutputStream("/tmp/test/Hello.class");
+        out.write(cw.toByteArray());
+
     }
 
 }
